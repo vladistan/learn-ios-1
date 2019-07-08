f21c214ab695ba2cf3cde12b31c28f88e603e248
vlad <vlad@v-lad.org>
2019-07-07 17:03:01 -0700

    Initial Commit

---
+++ Hello2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello2
+//
+//  Created by Vlad on 7/7/19.
+//  Copyright © 2019 Persyst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
